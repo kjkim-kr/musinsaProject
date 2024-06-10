82a009c7eb24d7627c8c17f4380c913e8ff1cfe3
kjkim <vore25@gmail.com>
2024-06-10 20:49:59 +0900

    2024.06.10 kjkim Brand, Category, Product Entity Class 추가

---
+++ src/main/java/com/kj/musinsaproject/product/Product.java
@@ -0,0 +1,25 @@
+package com.kj.musinsaproject.product;
+
+import com.kj.musinsaproject.brand.Brand;
+import com.kj.musinsaproject.category.Category;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private Double price;
+
+}
