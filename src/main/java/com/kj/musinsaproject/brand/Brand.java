691095e07102d4cefe7234dfd88dd8b24b55142b
kjkim <vore25@gmail.com>
2024-06-10 21:03:26 +0900

    2024.06.10 kjkim Brand, Category name (PK)로 설정

--- src/main/java/com/kj/musinsaproject/brand/Brand.java
+++ src/main/java/com/kj/musinsaproject/brand/Brand.java
@@ -1,9 +1,6 @@
 package com.kj.musinsaproject.brand;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Getter
@@ -13,5 +10,6 @@ public class Brand {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 }
--- src/main/java/com/kj/musinsaproject/category/Category.java
+++ src/main/java/com/kj/musinsaproject/category/Category.java
@@ -1,9 +1,6 @@
 package com.kj.musinsaproject.category;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Getter
@@ -13,5 +10,6 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 }
