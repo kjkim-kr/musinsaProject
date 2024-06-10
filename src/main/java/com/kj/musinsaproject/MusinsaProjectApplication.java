02502cfb9bedbd217fe7c4c05bf9ddeff2bc42f8
kjkim-kr <vore25@gmail.com>
2024-06-10 19:44:44 +0900

    Initial commit

---
+++ src/main/java/com/kj/musinsaproject/MusinsaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kj.musinsaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kj/musinsaproject/MusinsaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kj.musinsaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
